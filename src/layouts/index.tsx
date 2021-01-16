498fa74e1aebc0d185c8efab9ce6d48a81bfad32
yzh <1197926689@qq.com>
2021-01-16 14:38:45 +0800

    拖拽组件

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,12 @@
+import app from '@/pages/test1/test1';
 import React from 'react';
 import styles from './index.css';
+import App from '../pages/test1/test1'
 
 const BasicLayout: React.FC = props => {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+    <div>
+      <App />
       {props.children}
     </div>
   );
--- src/pages/test1/test1.tsx
+++ src/pages/test1/test1.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import styles from  "./drag.css";
+
+
 
 const app =()=>{
+
+const dragdiv = document.querySelector("#drag");
+
+const dragdivs = useRef();
+
+function onMouseDown(e:any){
+    let downX = e.clientX;
+    let downY = e.clientY;
+    let startMove = true;
+    let divRect = dragdiv.getBoundingClientRect();
+    
+    e.target.style.cursor = "move";
+
+    window.onmousemove = function(e){
+        if(!startMove){
+            return;
+        }
+        let newLeft = e.clientX - downX + divRect.left;
+        let newTop = e.clientY - downY + divRect.top;
+        dragdivs.current.style.left = newLeft+"px";
+        dragdivs.current.style.top = newTop+"px";
+        return false;
+    }
+
+    window.onmouseup = function(e){
+        startMove = false;
+        e.target.style.cursor = "default";
+    }
+}
+
+
+
+
     return (
-        <div>
-            <p>1111</p>
+        <div 
+        id="drag" 
+        style={{backgroundColor:"black",position:"absolute"}} 
+        className={styles.div1} 
+        onMouseDown={onMouseDown}
+        ref={dragdivs}
+        >
         </div>
     );
 };
